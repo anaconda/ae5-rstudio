1af835140511a0a037c006c84f7b3f17dbfcfd7c
Michael C. Grant <mgrant@anaconda.com>
2026-07-23 16:20:06 -0500

    Improve default_env.py further

--- default_env.py
+++ default_env.py
@@ -37,15 +37,32 @@ for ebase in ENVS_DIRS:
             g_envs.append((ver, ename))
     r_envs.extend(v for k, v in sorted(g_envs, reverse=True))
 
-results = []
+# This function is likely overkill but it is shared with
+# ae5-rstudio and ae5-vscode so we're offering the more
+# comprehensive search for safety (and it's cheap)
 
-try:
-    from anaconda_project.project_info import publication_info
-    spec = publication_info(PROJECT_DIR)
-    results.extend(spec['env_specs'])
-except Exception:
-    results.append('@ERROR@')
+def _ordered_environment_set(pdir):
+    env_names = []
+    def _add(x):
+        if x and x not in env_names:
+            env_names.append(x)
+    try:
+        from anaconda_project.project_info import publication_info
+        spec = publication_info(pdir)
+        for cspec in spec.get('commands', {}).values():
+            _add(cspec.get('env_spec'))
+        for ename in spec.get('env_specs', {}).keys():
+            _add(ename)
+    except Exception:
+        print('ERROR: {}'.format(exc), file=sys.stderr)
+        print('Could not parse pixi.toml/anaconda-project.yml.', file=sys.stderr)
+    root = os.environ.get('CONDA_ROOT')
+    if sys.prefix != root:
+        _add(sys.prefix)
+    _add('base')
+    return env_names
 
+results = _ordered_environment_set(PROJECT_DIR)
 desired_env = results[0]
 results = [r for r in results if r in r_envs]
 if results:
